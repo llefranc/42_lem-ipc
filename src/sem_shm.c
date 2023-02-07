e01675baec5e4f154ad97499470630c860be824d
Lucas Lefrancq <lucas.lefrancq@hotmail.fr>
2023-02-07 12:52:47 +0100

    add struct shmparam to centralize shm/sem infos

--- src/sem_shm.c
+++ src/sem_shm.c
@@ -6,12 +6,13 @@
 /*   By: llefranc <llefranc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 14:58:11 by llefranc          #+#    #+#             */
-/*   Updated: 2023/02/07 11:33:41 by llefranc         ###   ########.fr       */
+/*   Updated: 2023/02/07 12:50:30 by llefranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <errno.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 
 #include <sys/ipc.h>
 #include <sys/shm.h>
@@ -21,11 +22,36 @@
 #include "../include/sem_shm.h"
 
 /**
+ * init_shmparam() - Inits shmparam with all necessary information.
+ * @shm: Will contain all informations for a specific shm.
+ * @shm_key: Key used to identify the shm for shmget() call.
+ * @shm_size: Size expected in bytes for the shm.
+ * @sem_key: Key used to idendify the sem (obtained wtih semget() call)
+ * 		associated to the shm.
+ *
+ * Initializes the struct shmparam with all necessary info to identify and use
+ * a shm/sem couple. Allocates to smh->buf a tmp buffer used for read/write
+ * operations with shm.
+ *
+ * Return: 0 on success, -1 on malloc failure.
+*/
+int init_shmparam(struct shmparam *shm, int shm_key, int shm_size, int sem_key)
+{
+	memset((void *)shm, 0, sizeof(*shm));
+	shm->key = shm_key;
+	shm->size = shm_size;
+	shm->sem_key = sem_key;
+	if ((shm->buf = malloc(sizeof(*(shm->buf)) * shm->size)) == NULL) {
+		log_syserr("malloc");
+		return -1;
+	}
+	return 0;
+}
+
+/**
  * init_shm() - Inits a shared memory segment for this process.
- * @key: Key used to identified the shm for shmget() call.
- * @size: Size expected in bytes for the shm.
- * @shm_id: Set to id of the shm initialized after shmget() call.
- * @shm_addr: Set to the address used to access the shm afer shmat() call.
+ * @shm: Contains all informations for a specific shm. Needs to be initialized
+ * 		first with init_shmparam().
  *
  * Creates a shared memory segment based on SHM_KEY value if this one doesn't
  * exist (otherwise get a reference to it) and attaches it to this process.
@@ -34,24 +60,24 @@
  * Return: 0 if shared memory segment was correctly initialized and attached to
  * 		the process. -1 if one of the syscalls fail.
 */
-int init_shm(const int key, const int size, int *shm_id, char **shm_addr)
+int init_shm(struct shmparam *shm)
 {
 	void *tmp;
 	struct shmid_ds buf;
 
-	if ((*shm_id = shmget(key, size, IPC_CREAT | 0600)) == -1) {
+	if ((shm->id = shmget(shm->key, shm->size, IPC_CREAT | 0600)) == -1) {
 		log_syserr("(shmget)");
 		return -1;
 	}
-	if (shmctl(*shm_id, IPC_STAT, &buf) == -1) {
+	if (shmctl(shm->id, IPC_STAT, &buf) == -1) {
 		log_syserr("(shmctl)");
 		return -1;
 	}
-	if ((tmp = shmat(*shm_id, NULL, 0)) == (void*)-1) {
+	if ((tmp = shmat(shm->id, NULL, 0)) == (void*)-1) {
 		log_syserr("(shmat)");
 		return -1;
 	}
-	*shm_addr = (char *)tmp;
+	shm->addr = (char *)tmp;
 
 	log_verb("Shared memory segment initialized");
 	return 0;
@@ -59,28 +85,28 @@ int init_shm(const int key, const int size, int *shm_id, char **shm_addr)
 
 /**
  * clean_shm() - Detaches or removes a shared memory segment.
- * @shm_id: The id of the shared memory segment.
- * @shm_addr: The address used to access the shm.
+ * @shm: Contains all informations for a specific shm. Needs to be initialized
+ * 		first with init_shmparam().
  *
  * If this process is the last one attached to the shm, then removes the shm.
  * Otherwise just detaches this process from the shm, but doesn't removes it.
  *
  * Return: 0 on success, -1 on failure.
 */
-int clean_shm(int shm_id, char *shm_addr)
+int clean_shm(struct shmparam *shm)
 {
 	int nattch;
 
-	if ((nattch = get_shm_nattch(shm_id)) == -1)
+	if ((nattch = get_shm_nattch(shm->id)) == -1)
 		return -1;
 	if (nattch <= 1) {
-		if (shmctl(shm_id, IPC_RMID, NULL) == -1) {
+		if (shmctl(shm->id, IPC_RMID, NULL) == -1) {
 			log_syserr("(shmctl:IPC_RMID)");
 			return -1;
 		}
 		log_verb("Shared memory segment destroyed");
 	} else {
-		if (shmdt(shm_addr) == -1) {
+		if (shmdt(shm->addr) == -1) {
 			log_syserr("(shmdt)");
 			return -1;
 		}
