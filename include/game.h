eb4b5062e9a6cd9bd8deb54560b1b4f3b03ae310
Lucas Lefrancq <lucas.lefrancq@hotmail.fr>
2023-02-28 19:03:38 +0100

    add printmap function

--- include/game.h
+++ include/game.h
@@ -6,20 +6,24 @@
 /*   By: llefranc <llefranc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 18:28:07 by llefranc          #+#    #+#             */
-/*   Updated: 2023/02/27 18:28:36 by llefranc         ###   ########.fr       */
+/*   Updated: 2023/02/28 18:32:18 by llefranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GAME_H
 #define GAME_H
 
-#define NB_PLAYERS_MAX 10
-#define MAP_SIZE 100
+#define NB_PLAYERS_MAX 9
+#define NB_TEAMS_MAX 9
+#define MAP_NB_ROWS 10
+#define MAP_NB_COLUMNS 10
 
 struct mapinfo {
 	int nbp;
-	char nbp_team[NB_PLAYERS_MAX];
-	int map[MAP_SIZE];
+	char nbp_team[NB_TEAMS_MAX];
+	int map[MAP_NB_ROWS][MAP_NB_COLUMNS];
 };
+
+int print_map(const struct mapinfo *m);
 
 #endif /* GAME_H */
---
+++ src/game.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llefranc <llefranc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 16:06:56 by llefranc          #+#    #+#             */
+/*   Updated: 2023/02/28 18:53:05 by llefranc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <string.h>
+
+#include "../include/game.h"
+
+#include "../include/utils.h"
+
+int print_map(const struct mapinfo *m)
+{
+	static _Bool is_map_printed = 0;
+	char buf[(MAP_NB_COLUMNS * 2) + 2] = {}; /* +2 for 1st '\t' and '\0' */
+	int team_nb = -1;
+
+	if (is_map_printed) {
+		printf("\x1B[%dA", (MAP_NB_ROWS) + 2);
+		fflush(stdout); /* remove previous map from terminal */
+	}
+
+	printf("\n");
+	for (int row = 0; row < MAP_NB_ROWS; ++row) {
+		int i = -1;
+		memset(buf, 0, sizeof(buf));
+		buf[++i] = '\t';
+		for (int col = 0; col < MAP_NB_COLUMNS; ++col) {
+
+			/* Reading first byte only --> team id */
+			buf[++i] = (char)m->map[row][col] + '0';
+			buf[++i] = '|';
+		}
+		buf[i] = '\t';
+		printf("%s", buf); /* print line map */
+
+		if (++team_nb < NB_TEAMS_MAX) {
+			printf("Team %d: %d players", team_nb + 1,
+					(int)m->nbp_team[team_nb]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+	is_map_printed = 1;
+	return 0;
+}
