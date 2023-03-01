4543f5805c1a1d23de0781bb27d8a640c24e1175
Lucas Lefrancq <lucas.lefrancq@hotmail.fr>
2023-03-01 16:44:56 +0100

    add comments for function from game file

--- include/game.h
+++ include/game.h
@@ -6,7 +6,7 @@
 /*   By: llefranc <llefranc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 18:28:07 by llefranc          #+#    #+#             */
-/*   Updated: 2023/03/01 14:59:57 by llefranc         ###   ########.fr       */
+/*   Updated: 2023/03/01 16:41:01 by llefranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,41 @@
 
 #include "../include/shared_rcs.h"
 
+/**
+ * struct position - Represents a square on a grid.
+ * @row: Row number of the map.
+ * @col: Column number of the map.
+*/
 struct position {
 	int row;
 	int col;
 };
+
+/**
+ * struct mapinfo - Contains all the map information.
+ * @nbp: The number of player which joined the game since it started.
+ * @nbp_team: The number of players per team actually playing.
+ * @map: The game map containing all the players.
+ *
+ * All fields of mapinfo are stored in the shared memory segment, which allows
+ * each process to be able to see the actual game state and interact with it.
+*/
 struct mapinfo {
 	unsigned int nbp;
 	unsigned char nbp_team[NB_TEAMS_MAX];
 	unsigned int map[MAP_NB_ROWS][MAP_NB_COLUMNS];
 };
 
+/**
+ * struct player - Contains the player information.
+ * @id: The id of the player resulting in the combination of its personnal id,
+ *      which is the number of player who previously joined when he joined the
+ *      game, and its team id (3 bytes for player id, 1 byte for team id).
+ * @team_id: The team id of the player (lemipc arg, between 1 and 9).
+ * @targ_id: The id of an ennemy player that the player is actually targeting.
+ * @pos: The player position on the grid.
+ * @last_move: The time of when its last moved occured.
+*/
 struct player {
 	unsigned int id;
 	unsigned int team_id;
@@ -41,7 +66,7 @@ struct player {
 };
 
 
-int print_map(const struct mapinfo *m);
+void print_map(const struct mapinfo *m);
 struct position find_player_pos(const struct mapinfo *m, unsigned int id);
 int find_new_target(const struct shrcs *rcs, const struct mapinfo *m,
 		const struct player *p);
--- src/game.c
+++ src/game.c
@@ -6,17 +6,18 @@
 /*   By: llefranc <llefranc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 16:06:56 by llefranc          #+#    #+#             */
-/*   Updated: 2023/03/01 15:10:15 by llefranc         ###   ########.fr       */
+/*   Updated: 2023/03/01 16:44:33 by llefranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#include <errno.h>
-#include <limits.h>
 
 #include "../include/game.h"
 
@@ -25,7 +26,14 @@
 
 static inline _Bool is_tmate(int team_id, int play_id);
 
-int print_map(const struct mapinfo *m)
+/**
+ * print_map() - Refreshes the terminal with a representation of the game map.
+ * @m: Contains all the map information.
+ *
+ * Prints the map and teams infos on the terminal, or refreshes it with new
+ * infos in case it was already printed on the terminal.
+*/
+void print_map(const struct mapinfo *m)
 {
 	static _Bool is_map_printed = 0;
 	char buf[(MAP_NB_COLUMNS * 2) + 2] = {}; /* +2 for 1st '\t' and '\0' */
@@ -59,22 +67,45 @@ int print_map(const struct mapinfo *m)
 	printf("\n");
 
 	is_map_printed = 1;
-	return 0;
 }
 
-static int send_targ_id(int msgq_id, unsigned int team_id, unsigned int targ_id)
-{
-	int ret;
-	struct msgbuf buf = { .team_id = team_id };
-	*(unsigned int *)buf.targ_id = targ_id;
+/**
+ * send_targ_id() - Sends a target id on a message queue.
+ * @msqg_id: System V id of the message queue.
+ * @team_id: The team id of the player (lemipc arg, between 1 and 9).
+ * @targ_id: The id of a player to target resulting in the combination of its
+ *           personnal id and its team id (3 bytes for player id, 1 byte for
+ *           team id).
+ *
+ * Sends a target id associated to a team id on a message queue, that other
+ * players from same team can retrieve to target the same ennemy together.
+ *
+ * Return: 0 on success, -1 on failure.
+*/
+// static int send_targ_id(int msgq_id, unsigned int team_id, unsigned int targ_id)
+// {
+// 	int ret;
+// 	struct msgbuf buf = { .team_id = team_id };
+// 	*(unsigned int *)buf.targ_id = targ_id;
 
-	if ((ret = msgsnd(msgq_id, &buf, sizeof(buf.targ_id),
-			IPC_NOWAIT)) == -1) {
-		log_syserr("(msgsnd)");
-	}
-	return ret;
-}
+// 	if ((ret = msgsnd(msgq_id, &buf, sizeof(buf.targ_id),
+// 			IPC_NOWAIT)) == -1) {
+// 		log_syserr("(msgsnd)");
+// 	}
+// 	return ret;
+// }
 
+/**
+ * recv_targ_id() - Receives a target id from a message queue.
+ * @msqg_id: System V id of the message queue.
+ * @team_id: The team id of the player (lemipc arg, between 1 and 9).
+ *
+ * Receives a target id previously sent by another team member on a message
+ * queue in order to target all together the same ennemy.
+ *
+ * Return: target id if a team member previously sent one, 0 if no target id
+ *         for this specific team was on the message queue, -1 in case of error.
+*/
 static int recv_targ_id(int msgq_id, unsigned int team_id)
 {
 	struct msgbuf buf = { .team_id = team_id };
@@ -89,12 +120,35 @@ static int recv_targ_id(int msgq_id, unsigned int team_id)
 	return 0;
 }
 
+/**
+ * is_tmate() - Indicates if a player id belongs to a team or not.
+ * @team_id: The team id of the player (lemipc arg, between 1 and 9).
+ * @play_id: The id of a player resulting in the combination of its personnal id
+ *           and its team id (3 bytes for player id, 1 byte for team id).
+ *
+ * Return: 1 if the player belongs to the team, 0 otherwise.
+*/
 static inline _Bool is_tmate(int team_id, int play_id)
 {
 	return team_id == (char)play_id;
 }
 
-static unsigned int calc_dist(const struct mapinfo *m,
+/**
+ * calc_dist_to_ennemy() - Calculates the distance between the player and
+ *                         an ennemy.
+ * @m: Contains all the map information.
+ * @p: Contains the actual player information.
+ * @row: Row number of the map.
+ * @col: Column number of the map.
+ *
+ * Calculates the distance between the player and a map's position but only if
+ * it's an ennemy player which is standing on this square.
+ *
+ * Return: the distance in authorized moves (no diagonal moves) if it's an
+ *         ennemy standing on the square from row/col. UINT max if it's a
+ *         team member or if there is nobody.
+*/
+static unsigned int calc_dist_to_ennemy(const struct mapinfo *m,
 		const struct player *p, int row, int col)
 {
 	unsigned int dist;
@@ -107,6 +161,19 @@ static unsigned int calc_dist(const struct mapinfo *m,
 	return dist;
 }
 
+/**
+ * find_new_target() - Finds the id of next ennemy to target.
+ * @rcs: Contains all information for shared ressources.
+ * @m: Contains all the map information.
+ * @p: Contains the actual player information.
+ *
+ * Checks the message queue to see if a team member already sent the id of an
+ * ennemy player to target. If not, looks on the board to find the closest
+ * ennemy.
+ *
+ * Return: The identifiant of an ennemy player as a positive integer, 0 if there
+ *         is no ennemy to target, -1 if an error occured.
+*/
 int find_new_target(const struct shrcs *rcs, const struct mapinfo *m,
 		const struct player *p)
 {
@@ -119,7 +186,7 @@ int find_new_target(const struct shrcs *rcs, const struct mapinfo *m,
 
 	for (int row = 0; row < MAP_NB_ROWS; ++row) {
 		for (int col = 0; col < MAP_NB_COLUMNS; ++col) {
-			dist = calc_dist(m, p, row, col);
+			dist = calc_dist_to_ennemy(m, p, row, col);
 			if (closest > dist) {
 				closest = dist;
 				targ_id = m->map[row][col];
@@ -129,6 +196,15 @@ int find_new_target(const struct shrcs *rcs, const struct mapinfo *m,
 	return targ_id;
 }
 
+/**
+ * find_player_pos() - Finds a player position based on its id.
+ * @m: Contains all the map information.
+ * @id: The id of a player resulting in the combination of its personnal id
+ *      and its team id (3 bytes for player id, 1 byte for team id).
+ *
+ * Return: The position of the player if it exists on the map, or a position of
+ *         {-1,-1} if not.
+*/
 struct position find_player_pos(const struct mapinfo *m, unsigned int id)
 {
 	struct position p = { .row = -1, .col = -1};
